003bf11aa658e7f0f340c52ab67388b26d5c8e1c
Lmenuontop <Lmenuontop@proton.me>
2026-02-25 18:53:20 +0530

    Fix clock reference in update method

--- FTLEngine.py
+++ FTLEngine.py
@@ -28,7 +28,7 @@ class Engine:
 
     def update(self):
         pygame.display.flip()
-        clock.tick(60)
+        self.clock.tick(60)
 
     def handle_events(self):
         for event in pygame.event.get():
